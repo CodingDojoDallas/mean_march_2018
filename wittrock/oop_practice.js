5d884fd83baec9d8e00ab21fe1c8df78c52f5bc5
cole wittrock <cole.wittrock0@gmail.com>
2018-03-06 20:12:45 -0600

    3/6/18

--- wittrock/oop_practice.js
+++ wittrock/oop_practice.js
@@ -1,26 +1,20 @@
 function personConstructor(name, age) {
     // an object literal that will be returned
-    const person = {};
-    // attributes of a person
-    person.name = name;
-    person.age = age;
-    // when attached to an object or instance, functions are called 'methods'.
-    // this is our first method, greet
-    person.greet = function(){
-        console.log("Hello my name is " + person.name + " and I am " + person.age + " years old!");
+    this.name = name;
+    this.age = age;
+    this.greet=function(){
+        console.log("Hello my name is " + this.name + " and I am " + this.age + " years old!")
     }
-    // finally, this function must return an instance
-    return person;
 }
 // create the 'steve' instance, run greet
-const steve = personConstructor("Steve", 27);
+const steve = new personConstructor("Steve", 27);
 steve.greet();
 // create the 'anika' instance, run greet. note that it is different.
-const anika = personConstructor("Anika", 33);
+const anika = new personConstructor("Anika", 33);
 anika.greet();
 // finally note how we can refine the greet method for any particular instance
-const emily = personConstructor("Emily", 22);
+const emily = new personConstructor("Emily", 22);
 emily.greet = function() {
-    console.log("I am the greatest, ever!"+person.name);
-};
+    console.log("I am the greatest, ever!"+this.name);
+}
 emily.greet();
